715bda33f3f4cf66b442278f0a5aea4491956eec
Benjamin Johnson <bcfjohns@gmail.com>
2011-10-21 15:32:06 -0400

    Added code to play part of Handel when it needs input from me. had tried a different way of setting uAmplitudeEVen, which was inconsistent and now commented out. Not sure what if anything really changed in the model. I'm just committing it for consistencies sake. I did briefly have it animate things, but I turned it back off.

--- setUAmplitudeEven.m
+++ setUAmplitudeEven.m
@@ -1,5 +1,6 @@
 
 function setUAmplitudeEven(alpha)
+% uAmplitudeEven = zeros(1,3);
 global uAmplitudeEven
 mag = 2;
 %trying to assign angles, so a change in either will change z, when it's at
@@ -7,6 +8,5 @@ mag = 2;
 uAmplitudeEven(3) = mag*cos(alpha(2))*cos(alpha(1));
 uAmplitudeEven(1) = -mag*cos(alpha(2))*sin(alpha(1));
 uAmplitudeEven(2) = mag*sin(alpha(2));
-
-uAmplitudeEven = uAmplitudeEven
+% assignin('base', 'uAmplitudeEven', uAmplitudeEven);
 end
