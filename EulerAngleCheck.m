84b3b285b4827efd46d0ba3cf880999365153451
Benjamins Laptop <bcfjohns@mit.edu>
2012-01-19 11:23:31 -0500

    Changed rmToEAxyz to it's actually x-y-z convention (it used to be z-y-x convention). And made new file for z-y-x convention.

--- EulerAngleCheck.m
+++ EulerAngleCheck.m
@@ -1,87 +1,87 @@
 %find the general vector the spiral was along, so the angles can be found
 %and 'removed' So it's traveling along one of the axis.
-x = Pos1(60:end,1);
-y = Pos1(60:end,2);
-z = Pos1(60:end,3);
-
-% %remove means, so just looking at direction of motion;
-% x = x-mean(x);
-% y = y-mean(y);
-% z = z-mean(z);
-
-%Least squares line
-t = 1:length(x);
-A = [t' ones(length(x),1)];
-fitx = inv(A'*A)*A'*x;
-fity = inv(A'*A)*A'*y;
-fitz = inv(A'*A)*A'*z;
-plot(t, x)
-hold on;
-plot(t, t*fitx(1)+fitx(2), 'k');
-
-plot(y, 'r')
-plot(t, t*fity(1)+fity(2), 'm');
-
-plot(z, 'g')
-plot(t, t*fitz(1)+fitz(2), 'c');
-hold off;
-
-%rotate about x, such that y slope is 0;
-thetax = atan(fity(1)/fitz(1))*180/pi %=>thetax = 30.6671 with sim starting [0 0 0] x-y-z
-%=> 2.4748e-04 with sim starting [30.6671 0 0]
-%and thetay = 59.8797
-thetay = atan(fitx(1)/fitz(1))*180/pi
+% x = Pos1(60:end,1);
+% y = Pos1(60:end,2);
+% z = Pos1(60:end,3);
+% 
+% % %remove means, so just looking at direction of motion;
+% % x = x-mean(x);
+% % y = y-mean(y);
+% % z = z-mean(z);
+% 
+% %Least squares line
+% t = 1:length(x);
+% A = [t' ones(length(x),1)];
+% fitx = inv(A'*A)*A'*x;
+% fity = inv(A'*A)*A'*y;
+% fitz = inv(A'*A)*A'*z;
+% plot(t, x)
+% hold on;
+% plot(t, t*fitx(1)+fitx(2), 'k');
+% 
+% plot(y, 'r')
+% plot(t, t*fity(1)+fity(2), 'm');
+% 
+% plot(z, 'g')
+% plot(t, t*fitz(1)+fitz(2), 'c');
+% hold off;
+% 
+% %rotate about x, such that y slope is 0;
+% thetax = atan(fity(1)/fitz(1))*180/pi %=>thetax = 30.6671 with sim starting [0 0 0] x-y-z
+% %=> 2.4748e-04 with sim starting [30.6671 0 0]
+% %and thetay = 59.8797
+% thetay = atan(fitx(1)/fitz(1))*180/pi
 %%
 %take the list of rotation matrices convert to Euler angles for two salps
 %and plot the euler angles to compare them.
 EA1 = [];
 EA2 = [];
 for i = 1:length(RotM1)
-    EA1p = SpinCalc('DCMtoEA123', reshape(RotM1(i, :), 3,3), 0.1, 0);
+    EA1p = rmToEAxyz(reshape(RotM1(i, :), 3,3));
+%      EA1p = SpinCalc('DCMtoEA123', reshape(RotM1(i, :), 3,3), 0.1, 0);
     
-    EA1 = [angleN(EA1p); EA1];
+    EA1 = [EA1p; EA1];%[angleN(EA1p); EA1];
 %     
 %     EA2p = SpinCalc('DCMtoEA132', reshape(RotM2(i, :), 3,3), 0.1, 0);
 %     EA2 = [angleN(EA2p); EA2];
 %     
 end
 
+%pull out time vector from other data, so thing line up right
+time = Salp1_PandV.time;
 
-%%
+
+xmax = 750
 subplot(3,1,1)
-plot(EA1(:,1), 'r');
+plot(time, EA1(:,1), 'r');
 title('angle 1');
-% xlim([50 1250])
-% hold on;
-% plot(EA2(:,1), 'g');
-% hold off;
+xlim([0 xmax])
+xlabel('time (sec.)');
+ylabel('angle (degrees)');
 
 subplot(3,1,2)
-plot(EA1(:,2), 'r');
-xlim([50 1250])
+plot(time, EA1(:,2), 'r');
+xlim([0 xmax])
 title('angle 2');
-% hold on;
-% plot(EA2(:,2), 'g');
-% hold off;
+xlabel('time (sec.)');
+ylabel('angle (degrees)');
 
 subplot(3,1,3)
-plot(EA1(:,3), 'r');
+plot(time, EA1(:,3), 'r');
 title('angle 3');
-xlim([50 1250])
-% hold on;
-% plot(EA2(:,3), 'g');
-% hold off;
-
+xlim([0 xmax])
+xlabel('time (sec.)');
+ylabel('angle (degrees)');
 
 %%
 %Checking Angle conventions and ordering.
 clc
-t = pi/6;
+t = -pi/4;
 Rx = [1 0   0;
       0 cos(t) -sin(t);
       0 sin(t)  cos(t)];
 
-t = -pi/12;  
+t = -.3*pi/4;  
 Ry = [cos(t) 0 sin(t);
      0       1 0;
      -sin(t) 0  cos(t)];
@@ -110,33 +110,69 @@ EAz = SpinCalc('DCMtoEA123', Rz, 0.1, 1)
 % end
 
 EAxyz = SpinCalc('DCMtoEA123', Rz*Ry*Rx, 0.1, 0)
+t = EAxyz(1)*pi/180;
+Rx = [1 0   0;
+      0 cos(t) -sin(t);
+      0 sin(t)  cos(t)];
+
+t = EAxyz(2)*pi/180;
+Ry = [cos(t) 0 sin(t);
+     0       1 0;
+     -sin(t) 0  cos(t)];
+ 
+t = EAxyz(3)*pi/180;
+Rz = [cos(t) -sin(t) 0;
+      sin(t)  cos(t) 0;
+      0         0    1];
+
+EAxyz = SpinCalc('DCMtoEA123', Rz*Ry*Rx, 0.1, 0)
 
 %was testing my own code, but it wasn't quite working, but SpinCalc seems
 %to be confirmed working now.
-% % % EAx = rmToEAxyz(Rx)
-% % % EAy = rmToEAxyz(Ry)
-% % % EAz = rmToEAxyz(Rz)
-% % % EAxyz = rmToEAxyz(Rz*Ry*Rx)
+%%
+clc
+t = pi/4;
+Rx = [1 0   0;
+      0 cos(t) -sin(t);
+      0 sin(t)  cos(t)];
+
+t = -pi/6;  
+Ry = [cos(t) 0 sin(t);
+     0       1 0;
+     -sin(t) 0  cos(t)];
+ 
+
+t = 4*pi/3;
+Rz = [cos(t) -sin(t) 0;
+      sin(t)  cos(t) 0;
+      0         0    1];
+
+EAx = rmToEAxyz(Rx)
+EAy = rmToEAxyz(Ry)
+EAz = rmToEAxyz(Rz)
+EAxyz = rmToEAxyz(Rz*Ry*Rx)
 % % % %recalc to double check
-% % % t = EAxyz(1);
-% % % Rx = [1 0   0;
-% % %       0 cos(t) -sin(t);
-% % %       0 sin(t)  cos(t)];
-% % %   
-% % % t = EAxyz(2);
-% % % Ry = [cos(t) 0 sin(t);
-% % %      0       1 0;
-% % %      -sin(t) 0  cos(t)];
-% % % 
-% % % t = EAxyz(3);
-% % % Rz = [cos(t) -sin(t) 0;
-% % %       sin(t)  cos(t) 0;
-% % %       0         0    1];
-% % % 
-% % % EAx = rmToEAxyz(Rx)
-% % % EAy = rmToEAxyz(Ry)
-% % % EAz = rmToEAxyz(Rz)
-% % % EAxyz = rmToEAxyz(Rz*Ry*Rx)
+
+t = EAxyz(1)*pi/180;
+Rx = [1 0   0;
+      0 cos(t) -sin(t);
+      0 sin(t)  cos(t)];
+
+t = EAxyz(2)*pi/180;
+Ry = [cos(t) 0 sin(t);
+     0       1 0;
+     -sin(t) 0  cos(t)];
+ 
+t = EAxyz(3)*pi/180;
+Rz = [cos(t) -sin(t) 0;
+      sin(t)  cos(t) 0;
+      0         0    1];
+  
+  
+EAx = rmToEAxyz(Rx)
+EAy = rmToEAxyz(Ry)
+EAz = rmToEAxyz(Rz)
+EAxyz = rmToEAxyz(Rx*Ry*Rz)
 % % % 
 % % % %triple check
 % % % t = EAxyz(1);
--- rmToEAxyz.m
+++ rmToEAxyz.m
@@ -1,27 +1,30 @@
 function EA = rmToEAxyz(rm)
+%Takes a rotation matrix and returns three angles that would form rm via
+%Rz*Ry*Rx
+%code transformed from psuedo code on page 10 of 
+%http://www.geometrictools.com/Documentation/EulerAngles.pdf
 
 
-if (rm(1,3) < +1)
-    if (rm(1,3) > -1)
-        thetaY = asin(rm(1,3));
-        thetaX = atan2(-rm(2,3),rm(3,3));
-        thetaZ = atan2(-rm(1,2),rm(1,1));
+
+if (rm(3,1) < +1)
+    if (rm(3,1) > -1)
+        thetaY = asin(-rm(3,1));
+        thetaZ = atan2(rm(2,1),rm(1,1));
+        thetaX = atan2(rm(3,2),rm(3,3));
         
-    else    %rm(1,3)=-1
+    else % rm(3,1) = -1
         
-        % Not a unique solution:  thetaZ - thetaX = atan2(rm(2,1),rm(2,2))
-        thetaY = -pi/2;
-        thetaX = -atan2(rm(2,1),rm(2,2));
-        thetaZ = 0;
-        warn = 'no unique'
+        % Not a unique solution:  thetaX - thetaZ = atan2(-rm(2,3),rm(2,2))
+        thetaY = +pi/2;
+        thetaZ = -atan2(-rm(2,3),rm(2,2));
+        thetaX = 0;
     end
-else  % rm(1,3) = +1
+else % rm(3,1) = +1
     
-    % Not a unique solution:  thetaZ + thetaX = atan2(rm(2,1),rm(2,2))
-    thetaY = pi/2;
-    thetaX = atan2(rm(2,1),rm(2,2));
-    thetaZ = 0;
-    warn = 'no unique2'
+    % Not a unique solution:  thetaX + thetaZ = atan2(-rm(2,3),rm(2,2))
+    thetaY = -pi/2;
+    thetaZ = atan2(-rm(2,3),rm(2,2));
+    thetaX = 0;
 end
 
 EA = [thetaX thetaY thetaZ];
