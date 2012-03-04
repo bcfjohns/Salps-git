faf58244d2b5c7bd8a2ed8f617ea55865a53b7fc
Benjamins Laptop <bcfjohns@mit.edu>
2012-03-04 17:50:30 -0500

    Added and updateParams method n stochGradSalp.m that updates params based on a 5 component alpha. A single propulsion angle, 3 components for where the u-joint is with respect to the center of mass and then one angle for the constrained orientation between two slaps.

--- stochGradSalp.m
+++ stochGradSalp.m
@@ -1,7 +1,9 @@
 function [alphaHist valueHist] = stochGradSalp
 global uAmplitudeEven xFinal valueHist alphaHist Salp1_PandV Salp1_angles
 %set initial parameters for the gradient search
-    alpha = [0.0744 0.1201]; %the angles of the output force assume normalized to 4
+    alpha = randn(1,5);
+    %angle of propulsion, 3 components of direction to linkage (assuming
+    %front and back are symmetric. 1 angle for the relative rotation between two salps.
     
     stand_dev_beta = 0.1;
     etta = 500;
@@ -12,13 +14,14 @@ global uAmplitudeEven xFinal valueHist alphaHist Salp1_PandV Salp1_angles
     valueHist = zeros(1, maxI);    
     
     for i = 1:maxI
-%         tic
         i = i
         alpha = alpha
         etta = etta*0.99
         %================================================================
         %sim with alpha then compute J_alpha
-        setUAmplitudeEven(alpha)
+        updateParams(alpha);
+        
+        
       
         tic
         sim('salpChain');
@@ -28,7 +31,7 @@ global uAmplitudeEven xFinal valueHist alphaHist Salp1_PandV Salp1_angles
         
         %================================================================
         %sim with alpha+beta then compute J_alpha
-        beta = stand_dev_beta*randn(sizeBeta);
+        beta = stand_dev_beta.*randn(sizeBeta);
         
         setUAmplitudeEven(alpha+beta)
         tic
@@ -71,102 +74,57 @@ global uAmplitudeEven xFinal valueHist alphaHist Salp1_PandV Salp1_angles
     end
 end
 
+function updateParams(alpha)  
+global connectR frontConnect backConnect u1axis u2axis
 
-% % Moved to it's own file, for modularities sake.
-% % function cost = valueFunction()
-% % global Salp1_PandV Salp1_angles
-% % 
-% % %find steady state indices based on angles
-% % T = 50*5; %at least one period to define the range we'll use.
-% % margin = 0.1; %what margin to use, so don't cut data off if just a little different
-% % time = Salp1_angles.time;
-% % angle2 = Salp1_angles.signals(2).values;
-% % angle1 = Salp1_angles.signals(1).values;
-% % L = length(angle1);
-% % 
-% % 
-% % min1 = min(angle1(L-T:L));
-% % min1 = min1-margin*range(min1);
-% % 
-% % max1 = max(angle1(L-T:L));
-% % max1 = max1+margin*range(max1);
-% % 
-% % min2 = min(angle2(L-T:L));
-% % min2 = min2-margin*range(min2);
-% % 
-% % max2 = max(angle2(L-T:L));
-% % max2 = max2+margin*range(max2);
-% % 
-% % smallestIndex = L-T;
-% % 
-% % jump = T;
-% % while(smallestIndex > T)
-% % angle1Part = angle1(smallestIndex-jump:smallestIndex);
-% % angle2Part = angle2(smallestIndex-jump:smallestIndex);
-% %     if(min(angle1Part) > min1 && min(angle2Part) > min2 && ...
-% %             max(angle1Part) < max1 && max(angle2Part) < max2)
-% %         smallestIndex = smallestIndex-jump;
-% %     else
-% %         break;
-% %     end
-% % end
-% % 
-% % figure(1)
-% % plot(time(1:smallestIndex), angle1(1:smallestIndex), 'r');
-% % hold on;
-% % plot(time(smallestIndex: L), angle1(smallestIndex:L), 'b');
-% % plot(time, min1*ones(L,1), '--k');
-% % plot(time, max1*ones(L,1), '--k');
-% % hold off;
-% % title('angle1');
-% % axis([0 time(L) min1-0.1 max1+0.1]);
-% % 
-% % figure(2);
-% % plot(time(1:smallestIndex), angle2(1:smallestIndex), 'r');
-% % hold on;
-% % plot(time(smallestIndex: L), angle2(smallestIndex:L), 'b');
-% % plot(time, min2*ones(L,1), '--k');
-% % plot(time, max2*ones(L,1), '--k');
-% % hold off;
-% % title('angle2');
-% % axis([0 time(L) min2-0.1 max2+0.1]);
-% % 
-% % 
-% % % velocities = Salp1_PandV.signals(2).values(smallestIndex:L, :);
-% % % velocity = sqrt(sum(velocities.^2,2));
-% % % avgVeloc = mean(velocity);
-% % % cost = -avgVeloc;
-% % positions = Salp1_PandV.signals(2).values([smallestIndex L],:);
-% % dpos = diff(positions)/(L-smallestIndex+1);
-% % cost = -norm(dpos);
-% % end
+    %Check that the angles are withing appropriate bounds.
+    angle1bound = pi/2.5;
+    if (alpha(1)>angle1bound)
+        alpha(1)=angle1bound)
+    else if (alpha(1)<0)
+            alpha(1) = 0;
+        end
+    end
+    
+    alpha(5)= mod(alpha(5),angle2bound);
+    
+    
+    %update propulsion based on angle.
+    setUAmplitudeEven([alpha(1) 0])
+    %using this angle puts gives the propulsion x and z hat components,
+    %which corresponds with a necessary change in sign in the model file
+    %for the x hat component.
 
 
-function cost = valueFunctionOld(finalState, currentAlpha)
-    %just distance moved since beginning.
-%     [xName yName zName] = finalState.signals(15:17).values;
-%     %make sure getting the right states
-%     if (~(strcmp(xName,'SixDoF.P1.Position') && strcmp(yName, 'SixDoF.P2.Position') && ...
-%             strcmp(zName, 'SixDoF.P3.Position')))
-        pos = finalState(15:17);
-        value = norm(pos);
-        cost = -value;
-%     else
-%         warning('Got wrong state variables for value function');
-%     end
+    %update vectors for where uJoint is connected.
+    frontConnect = [alpha(2) alpha(3) alpha(4)];
+    backConnect = [-alpha(2) -alpha(3) alpha(4)];
+
+    %update u joint and things, so constrained orientation axis is
+    %always along the axis of connection.
+
+    v = backConnect;
+    t = atan2(v(1),v(3));  
+    Ry = [cos(t) 0 -sin(t);
+         0       1 0;
+         sin(t) 0  cos(t)];
+
+    v2 = Ry*v;
+
+    t = atan2(v2(2),v2(3));
+    Rx = [1 0   0;
+          0 cos(t) -sin(t);
+          0 sin(t)  cos(t)];
+    %vFinal = Rx*v2; %This should always be in the z hat directions
+    
+    %Rotation matix from backConnects direction to z hat direction
+    Rspec_z = Rx*Ry;
+    %rotation matrix for z direction to backConnect's direction
+    R = Rspec_z';
+    %rotate axis on u joint
+    u1axis = [1 0 0]*R;
+    u2axis = [0 1 0]*R;
+    %rotate angle for fixed orientation
+    connectR = 180/pi*[0 0 alpha(5)]*R;
 end
-% % Moved to it's own file for modularities sake.
-% % function setUAmplitudeEven(alpha)
-% % global uAmplitudeEven
-% % mag = 2;
-% % %trying to assign angles, so a change in either will change z, when it's at
-% % %the singularity point.
-% % uAmplitudeEven(3) = mag*cos(alpha(2))*cos(alpha(1));
-% % uAmplitudeEven(1) = -mag*cos(alpha(2))*sin(alpha(1));
-% % uAmplitudeEven(2) = mag*sin(alpha(2));
-% % 
-% % uAmplitudeEven = uAmplitudeEven
-% % 
-% % end
-% %     
     
