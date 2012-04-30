3992fbd4c9d01e9ad9692123afc84e8d53d611a4
Benjamins Laptop <bcfjohns@mit.edu>
2012-04-30 14:17:06 -0400

    Added axis labels to smallEtta15runsPlot

--- smallEtta15runsPlot.m
+++ smallEtta15runsPlot.m
@@ -1,8 +1,13 @@
 close all;
 colors = ['b', 'g', 'r', 'c', 'm', 'y', 'k', 'b']
-for ii =1:15
+for ii =3
     ii
 plot3(historyAll(1,:,ii), historyAll(2,:,ii),historyAll(3,:,ii),[colors(mod(ii,8)+1) '.']);
+title('points visited by 15 runs of the optimization');
+xlabel('alpha 1');
+ylabel('alpha 2');
+zlabel('cost');
+
 hold on;
 end
 hold off;
